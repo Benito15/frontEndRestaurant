366c5b3731802c8308bfb06767dd2adee75870bf
Benito15 <b.kriel34@gmail.com>
2019-10-16 10:10:04 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/ui/nav/nav.component';
+import { GuestListComponent } from './components/guest/guest-list/guest-list.component';
+import { CreateGuestComponent } from './components/guest/create-guest/create-guest.component';
+import { EditGuestComponent } from './components/guest/edit-guest/edit-guest.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    GuestListComponent,
+    CreateGuestComponent,
+    EditGuestComponent
   ],
   imports: [
     BrowserModule,
